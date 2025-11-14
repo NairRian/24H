75fbd237d85fb75ebe4c2b038f2bc44aa40d0cdb
NairRian <166063685+NairRian@users.noreply.github.com>
2025-11-14 04:37:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title("Construis ta pyramide !")
 
 # --- Inputs utilisateur ---
 volume_pierre = st.slider(
-    "Quel volume de pierre pour construire l'extérieur de ta pyramide ? (en milliers de m3)",
+    f"Quel volume de pierre pour construire l'extérieur de ta pyramide ? (en milliers de m3)\n(min: 10, max: 100)",
     min_value=10,
     max_value=100,
     value=40
@@ -22,9 +22,25 @@ methode_extrac = st.selectbox(
     ["Levage", "Dragage", "Explosifs"]
 )
 
-# --- Bouton pour déclencher le calcul ---
-if st.button("Soumettre données"):
-    # --- Logique du choix d'image ---
+# --- Bouton centré et rouge ---
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    submit = st.button("Soumettre données", key="submit", help="Clique pour afficher les résultats")
+    st.markdown("""
+        <style>
+        div.stButton > button:first-child {
+            background-color: red;
+            color: white;
+            width: 100%;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+# --- Affichage des résultats ---
+if submit:
+    st.markdown("## Résultats de ta pyramide")
+
+    # Logique du choix d'image
     if volume_pierre < 60:
         image = "no_pyramide.jpeg"
     elif volume_pierre > 80:
@@ -42,17 +58,18 @@ if st.button("Soumettre données"):
             else:
                 image = "bravo.jpeg"
 
-    # --- Affichage des résultats ---
-    st.write(f"Votre volume de pierre : {volume_pierre} milliers de m3")
-    st.write(f"Type de roche choisi : {type_roche}")
-    st.write(f"Méthode d'extraction : {methode_extrac}")
+    # Affichage centré avec texte en gras
+    st.markdown(f"<p style='text-align:center;'>Votre volume de pierre : <b>{volume_pierre}</b> milliers de m3</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align:center;'>Type de roche choisi : <b>{type_roche}</b></p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align:center;'>Méthode d'extraction : <b>{methode_extrac}</b></p>", unsafe_allow_html=True)
 
-    # --- Affichage de l'image ---
+    # Affichage image centrée
     img_path = os.path.join("model", image)
     if os.path.exists(img_path):
+        fig, ax = plt.subplots()
         img = plt.imread(img_path)
-        plt.imshow(img)
-        plt.axis("off")
-        st.pyplot(plt)
+        ax.imshow(img)
+        ax.axis("off")
+        st.pyplot(fig)
     else:
         st.warning(f"Image {image} introuvable dans le dossier 'model/'.")
